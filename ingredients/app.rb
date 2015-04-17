b2f271f786dd3ba8cb0597d0dbdbed594dc06dc4
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2015-04-17 17:29:08 +0200

    feat(ingredient): implemented single get and update

--- ingredients/app.rb
+++ ingredients/app.rb
@@ -10,6 +10,14 @@ helpers do
 
     JSON.parse body
   end
+
+  def load_ingredient
+    begin
+      Ingredient.find params[:id]
+    rescue Elasticsearch::Persistence::Repository::DocumentNotFound
+      halt 404, {error: 'Ingredient not found.'}.to_json
+    end
+  end
 end
 
 post '/' do
@@ -27,17 +35,25 @@ get '/' do
   if query
     results = Ingredient.search query: {match: {name: query}}, size: 18
   else
-    results = Ingredient.search query: {match_all: {}}, size: 6
+    results = Ingredient.search query: {match_all: {}}, sort: {created_at: {order: 'desc'}}, size: 18
   end
   results.to_json
 end
 
+get '/:id' do
+  load_ingredient.to_json
+end
+
+put '/:id' do
+  ingredient = load_ingredient
+  data = input_request
+  ingredient[:name] = data['name']
+  ingredient[:group] = data['group']
+  ingredient[:nutrients] = data['nutrients']
+  ingredient.save
+  halt 200
+end
+
 delete '/:id' do
-  begin
-    ingredient = Ingredient.find params[:id]
-    ingredient.destroy
-    halt 200
-  rescue Elasticsearch::Persistence::Repository::DocumentNotFound
-    halt 404, {error: 'Ingredient not found.'}.to_json
-  end
+  load_ingredient.destroy
 end
