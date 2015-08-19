ecc6b3afbec52daca7022719efcc887df0922a72
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2015-08-19 14:39:41 +0200

    feat(users): refactored Twitter sign in.

--- app/users/auth_api.rb
+++ app/users/auth_api.rb
@@ -1,8 +1,6 @@
 require 'active_support/core_ext/object'
 require 'sinatra/base'
 require 'newrelic_rpm'
-require 'twitter_oauth'
-require 'redis'
 require_relative '../api_helpers'
 require_relative 'oauth'
 require_relative 'user'
@@ -12,62 +10,46 @@ class AuthAPI < Sinatra::Base
 
   helpers ApiHelpers
 
-  %w{facebook google}.each do |provider|
-    post "/#{provider}" do
-      oauth = "Oauth::#{provider.capitalize}Client".constantize.new
-      if oauth.authorized?(parse_request)
-        user = User.find_by_oauth(provider, oauth.profile.provider_id)
-        if user.blank?
-          user = User.find_by_email(oauth.profile.email)
-          if user.blank?
-            user = User.new
-            user.email = oauth.profile.email
-            user.password = SecureRandom.hex
-          end
-        end
-        user.display_name = oauth.profile.display_name
-        user.avatar = oauth.profile.avatar
-        user[provider] = oauth.profile.provider_id
-        user.save
-        halt 200, {token: Token.encode(user.id)}.to_json
-      end
-      halt 401, {error: 'Authentication failed'}.to_json
-    end
+  post '/facebook' do
+    sign_in 'facebook'
+  end
+
+  post '/google' do
+    sign_in 'google'
   end
 
   post '/twitter' do
     request = parse_request
-    twitter = TwitterOAuth::Client.new(consumer_key: ENV['TWITTER_KEY'], consumer_secret: ENV['TWITTER_SECRET'])
-    redis = Redis.new
     oauth_token = request['oauth_token']
 
     if oauth_token.blank?
-      token = twitter.request_token({oauth_callback: request['redirectUri']})
-      redis.set(token.token, token.secret)
-      halt 200, {oauth_token: token.token}.to_json
-    else
-      if oauth_token && request['oauth_verifier']
-        access_token = twitter.authorize(oauth_token, redis.get(oauth_token), oauth_verifier: request['oauth_verifier'])
-        redis.set(access_token.token, access_token.secret)
-        twitter = TwitterOAuth::Client.new(
-            consumer_key: ENV['TWITTER_KEY'],
-            consumer_secret: ENV['TWITTER_SECRET'],
-            token: access_token.token,
-            secret: access_token.secret
-        )
-        profile = twitter.info
-
-        user = User.find_by_oauth(:twitter, profile['id'])
-        if user.blank?
-          user = User.new({display_name: profile['screen_name'], password: SecureRandom.hex})
-          user.avatar = profile['profile_image_url']
-          user.twitter = profile['id']
-          user.save
-        end
-
-        halt 200, {token: Token.encode(user.id)}.to_json
-      end
+      client = Oauth::TwitterClient.new
+      halt 200, {oauth_token: client.get_request_token(request['redirectUri'])}.to_json
+    elsif oauth_token && request['oauth_verifier']
+      sign_in('twitter', request)
     end
   end
 
+  # Sign in with given provider and request.
+  def sign_in(provider, request = nil)
+    request ||= parse_request
+    oauth = "Oauth::#{provider.capitalize}Client".constantize.new
+    if oauth.authorized?(request)
+      user = User.find_by_oauth(provider, oauth.profile.provider_id)
+      if user.blank?
+        user = User.find_by_email(oauth.profile.email)
+        if user.blank?
+          user = User.new
+          user.email = oauth.profile.email
+          user.password = SecureRandom.hex
+        end
+      end
+      user.display_name = oauth.profile.display_name
+      user.avatar = oauth.profile.avatar
+      user[provider] = oauth.profile.provider_id
+      user.save
+      halt 200, {token: Token.encode(user.id)}.to_json
+    end
+    halt 401, {error: 'Authentication failed'}.to_json
+  end
 end
---
+++ app/users/oauth/twitter.rb
@@ -0,0 +1,46 @@
+require 'twitter_oauth'
+require 'redis'
+
+module TwitterOAuth
+  class Client
+    # Override TwitterOAuth gem method to get user email address.
+    def info
+      get('/account/verify_credentials.json?include_email=true')
+    end
+  end
+end
+
+module Oauth
+  class TwitterClient < Oauth::Base
+    def initialize
+      @redis = Redis.new
+      @client = TwitterOAuth::Client.new(consumer_key: ENV['TWITTER_KEY'], consumer_secret: ENV['TWITTER_SECRET'])
+    end
+
+    def get_request_token(redirect_uri)
+      token = @client.request_token({oauth_callback: redirect_uri})
+      @redis.set(token.token, token.secret)
+      token.token
+    end
+
+    def authorized?(params)
+      access_token = @client.authorize(
+          params['oauth_token'],
+          @redis.get(params['oauth_token']),
+          oauth_verifier: params['oauth_verifier']
+      )
+      authorized_client = TwitterOAuth::Client.new(
+          consumer_key: ENV['TWITTER_KEY'],
+          consumer_secret: ENV['TWITTER_SECRET'],
+          token: access_token.token,
+          secret: access_token.secret
+      )
+      twitter = authorized_client.info
+      @profile = Oauth::Profile.new
+      @profile.provider_id = twitter['id']
+      @profile.display_name = twitter['screen_name']
+      @profile.email = twitter['email']
+      @profile.avatar = twitter['profile_image_url']
+    end
+  end
+end
