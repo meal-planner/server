14afeebea2afb61042151473195e45b3dfb1b763
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2015-09-12 19:43:47 +0200

    devops: fix deployment server name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server 'meal-planner.org', user: 'deployer', roles: %w{app}
+server 'api.meal-planner.org', user: 'deployer', roles: %w{app}
 
 # Configuration
 # =============
