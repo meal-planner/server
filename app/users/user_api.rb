551549c0f4fa0d8620271d58d235af44a1afe5f8
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2015-09-20 14:05:31 +0200

    feat(user): add signup

--- app/users/user_api.rb
+++ app/users/user_api.rb
@@ -9,6 +9,21 @@ class UserAPI < Sinatra::Base
   post '/signup' do
     request = parse_request
 
+    user = UserRepository.find_by_email(request[:email])
+    halt 400, {error: "Email #{request[:email]} is already registered."}.to_json if user.present?
+
+    user = UserRepository.klass.new
+    user.email = request[:email]
+    user.display_name = request[:display_name]
+    user.password = request[:password]
+    validator = UserValidator.new
+    halt 422, validator.errors.to_json unless validator.valid?(user)
+
+    UserRepository.save user
+    mailer = UserMailer.new user
+    mailer.send_welcome_email
+
+    halt 200, {token: Token.encode(user.id)}.to_json
   end
 
   post '/login' do
