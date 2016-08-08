5df0badb03cbb0ac4438e48494755accf5efb87a
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-08-08 12:13:03 +0200

    docs: setup apidoc

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        apidoc: {
+            myapp: {
+                src: "app/",
+                dest: "_doc/"
+            }
+        },
+        buildcontrol: {
+            options: {
+                dir: '_site',
+                commit: true,
+                push: true,
+                message: 'Built Meal Planner API docs from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            github: {
+                options: {
+                    remote: 'git@github.com:meal-planner/server.git',
+                    branch: 'gh-pages'
+                }
+            }
+        },
+    });
+
+    grunt.registerTask('deploy', [
+        'apidoc',
+        'buildcontrol:github'
+    ]);
+
+    grunt.loadNpmTasks('grunt-apidoc');
+    grunt.loadNpmTasks('grunt-build-control');
+};
