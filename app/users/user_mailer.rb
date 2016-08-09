61eff56abdffb75a5452603c117cf9c35dd1898f
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-08-09 14:06:46 +0200

    users: update to latest send grid API

--- app/users/user_mailer.rb
+++ app/users/user_mailer.rb
@@ -1,3 +1,5 @@
+include SendGrid
+
 # Sends emails to user
 class UserMailer
   def initialize(user)
@@ -6,7 +8,7 @@ class UserMailer
 
   def send_welcome_email
     send_email(
-      subject: 'Welcome to Meal-Planner.org!',
+      subject:     'Welcome to Meal-Planner.org!',
       template_id: 'f98eacca-1e2f-4e76-bd36-86c4d0dc35da'
     )
   end
@@ -14,47 +16,47 @@ class UserMailer
   def send_password_reset_email
     reset_link = "#{ENV['CLIENT_URL']}#/reset-password/#{@user.password_token}"
     send_email(
-      subject: 'Password reset request',
+      subject:     'Password reset request',
       template_id: '9b2e2975-7869-44c4-9ef5-8532e57cb79f',
-      subs: {
+      subs:        {
         '-password_reset_link-' => reset_link
       }
     )
   end
 
   private
-
   def send_email(subject: nil, template_id: nil, subs: {})
-    client = SendGrid::Client.new(
-      api_user: ENV['SENDGRID_USERNAME'],
-      api_key: ENV['SENDGRID_PASSWORD']
+    mail                  = build_mail(subject, template_id)
+    mail.personalizations = personalize(subs)
+
+    send_grid = SendGrid::API.new(
+      api_key: ENV['SENDGRID_KEY'],
+      host:    'https://api.sendgrid.com'
     )
-    mail = build_mail subject
-    mail.smtpapi = build_header(template_id, subs)
-    client.send mail
+    send_grid.client.mail._('send').post(request_body: mail.to_json)
   end
 
-  def build_mail(subject)
-    SendGrid::Mail.new do |m|
-      m.to = @user.email
-      m.from = 'robot@meal-planner.org'
-      m.from_name = 'Meal Planner'
-      m.subject = subject
-      m.text = '&nbsp;'
-      m.html = '&nbsp;'
-    end
+  private
+  def build_mail(subject, template_id)
+    mail             = Mail.new
+    mail.template_id = template_id
+    mail.subject     = subject
+    mail.from        = Email.new(
+      email: 'robot@meal-planner.org',
+      name:  'Meal Planner'
+    )
+    mail
   end
 
-  def build_header(template_id, subs)
-    header = Smtpapi::Header.new
+  private
+  def personalize(subs)
+    personalization    = Personalization.new
+    personalization.to = Email.new(email: @user.email)
     if subs
       subs.each do |k, v|
-        header.add_substitution(k, Array(v))
+        personalization.substitutions = Substitution.new(key: k, value: v)
       end
     end
-    header.add_filter('templates', 'enable', 1)
-    header.add_filter('templates', 'template_id', template_id)
-
-    header
+    personalization
   end
 end
